1df5f5d9fc5b3f7456ac4af861b7fa43d5e120e7
Vinh nheo <vinh.lamminh@hcmut.edu.vn>
2022-12-17 00:01:14 +0700

    upload UART function  - checkout not complete in lab3

---
+++ traffic_light_app/Core/Inc/main.h
@@ -0,0 +1,111 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2022 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f1xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define RED_ROAD1_Pin GPIO_PIN_1
+#define RED_ROAD1_GPIO_Port GPIOA
+#define YELLOW_ROAD1_Pin GPIO_PIN_2
+#define YELLOW_ROAD1_GPIO_Port GPIOA
+#define GREEN_ROAD1_Pin GPIO_PIN_3
+#define GREEN_ROAD1_GPIO_Port GPIOA
+#define RED_ROAD2_Pin GPIO_PIN_4
+#define RED_ROAD2_GPIO_Port GPIOA
+#define YELLOW_ROAD2_Pin GPIO_PIN_5
+#define YELLOW_ROAD2_GPIO_Port GPIOA
+#define GREEN_ROAD2_Pin GPIO_PIN_6
+#define GREEN_ROAD2_GPIO_Port GPIOA
+#define EN0_Pin GPIO_PIN_7
+#define EN0_GPIO_Port GPIOA
+#define BUTTON_1_Pin GPIO_PIN_0
+#define BUTTON_1_GPIO_Port GPIOB
+#define BUTTON_2_Pin GPIO_PIN_1
+#define BUTTON_2_GPIO_Port GPIOB
+#define BUTTON_3_Pin GPIO_PIN_2
+#define BUTTON_3_GPIO_Port GPIOB
+#define b_7SEG_Pin GPIO_PIN_10
+#define b_7SEG_GPIO_Port GPIOB
+#define c_7SEG_Pin GPIO_PIN_11
+#define c_7SEG_GPIO_Port GPIOB
+#define d_7SEG_Pin GPIO_PIN_12
+#define d_7SEG_GPIO_Port GPIOB
+#define e_7SEG_Pin GPIO_PIN_13
+#define e_7SEG_GPIO_Port GPIOB
+#define f_7SEG_Pin GPIO_PIN_14
+#define f_7SEG_GPIO_Port GPIOB
+#define g_7SEG_Pin GPIO_PIN_15
+#define g_7SEG_GPIO_Port GPIOB
+#define EN1_Pin GPIO_PIN_8
+#define EN1_GPIO_Port GPIOA
+#define EN2_Pin GPIO_PIN_11
+#define EN2_GPIO_Port GPIOA
+#define EN3_Pin GPIO_PIN_12
+#define EN3_GPIO_Port GPIOA
+#define a_7SEG_Pin GPIO_PIN_9
+#define a_7SEG_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
